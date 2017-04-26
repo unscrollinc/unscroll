791802337cde8f703b6b37dd1bc795d337298112
Paul Ford <ford@ftrain.com>
2017-04-26 07:20:40 -0400

    Further rearrange work.

--- unscroll/scrolls/static/scrolls/rearrange.js
+++ unscroll/scrolls/static/scrolls/rearrange.js
@@ -9,7 +9,8 @@
 })(jQuery);
 
 $(document).ready(function() {
-    var max=2000000000.0;      
+    var max = 200.0;
+    const SUBTRACT = -1;
     var moving = false;
     var elToMove = undefined;
     var elsToMove = [];
@@ -26,26 +27,30 @@ $(document).ready(function() {
 	mover.on('click', function(e) {
 	    if(!moving) {
 		e.stopPropagation();
-		el.css({'opacity':'0.2'});
+		el.css({'opacity':0.1});
 		$('.mover')
 		    .html('R')
 		    .on('click', function(ev) {
 			ev.stopPropagation();
 			var p = $(this).parent();
-
 			if (elToMove.isBefore(p)) {
-			    elsToMove = $(elToMove)
+			    elsToMove = [$(elToMove)];
+			    var els = $(elToMove)
 				.nextUntil(p)
 				.add(p)
-				.css({'opacity':0.4});
+				.addBack()			    
+				.css({'opacity':0.1});
+			    elsToMove.push.apply(elsToMove, els);
 			}
-			else {
-			    elsToMove = $(p)
+			if (p.isBefore(elToMove)) {
+			    elsToMove = [$(elToMove)];			    
+			    $(p)
 				.nextUntil(elToMove)
 				.add(p)
-				.css({'opacity':0.4});
+				.addBack()			    
+				.css({'opacity':0.1});
+			    elsToMove.push.apply(els, elsToMove);			    
 			}
-			
 			console.log('Clicked on range. from:',
 				    elToMove.data('order'),
 				    'to: ',
@@ -65,24 +70,58 @@ $(document).ready(function() {
     
     function addClicker(item) {
 	item.on('click', function(e){
-	    if(moving) {
-		if (elToMove !== item) {
-		    var old_id = elToMove.data('order');
+	    if (moving) {
+		var ct = elsToMove.length;
+		if (ct > 0) {
 		    var rightBefore = item.prev().data('order');
 		    var rightAfter = item.data('order');
-		    var between = rightBefore + (rightAfter - rightBefore)/2;			    
-		    if (rightBefore===undefined) {
-			between = rightAfter - 1;
+		    console.log('count is', ct);
+		    for (var i = 0; i<ct; i++) {
+			var el = $(elsToMove[i]);
+			var between = rightBefore + ((i + 1) * ((rightAfter - rightBefore)/(1 + ct)));
+			old_order = el.data('order');
+			el.data('order', between);
+			el.children('p.no').remove();
+			el.prepend('<p class="no"><b>X' + between + '</b></p>');
+			el.insertBefore(item);
+			el.css({'opacity':'1'});
+
+			console.log('EACH: PATCH this note with ID X with an update that the order',
+				    old_order, 'is now', between);
+
 		    }
-		    elToMove.data('order', between);
-		    elToMove.children('p.no').remove();
-		    elToMove.prepend('<p class="no"><b>' + between + '</b></p>');
-		    elToMove.insertBefore(item);
+			
+		    $('.mover').html('M');			
+		    moving = false;		
+		    elToMove = undefined;
+		    elsToMove = [];
+
+		    
 		}
-		elToMove.css({'opacity':'1'});
-		$('.mover').html('M');
-		moving = false;
-		console.log('PATCH this note with ID X with an update that the order', old_id, 'is now', between);
+		else {
+		    if (elToMove !== item) {
+			var old_id = elToMove.data('order');
+			var rightBefore = item.prev().data('order');
+			var rightAfter = item.data('order');
+			var between = rightBefore + (rightAfter - rightBefore)/2;			    
+			if (rightBefore===undefined) {
+			    between = rightAfter - 1;
+			}
+			elToMove.data('order', between);
+			elToMove.children('p.no').remove();
+			elToMove.prepend('<p class="no"><b>' + between + '</b></p>');
+			elToMove.insertBefore(item);
+		    }
+		    elToMove.css({'opacity':'1'});
+		    $('.mover').html('M');
+		    $('.mover').html('M');			
+		    moving = false;		
+		    elToMove = undefined;
+		    elsToMove = [];			
+
+		    console.log('PATCH this note with ID X with an update that the order', old_id, 'is now', between);
+		}
+
 	    }
 	});
     }
@@ -105,7 +144,9 @@ $(document).ready(function() {
 	var item = $('<div></div>',
 		     {'class':cssClass})
 		     .html(text)
-	    .css({'background':"#"+((1<<24)*Math.random()|0).toString(16)});
+	    .css({'background':"#"+((1<<24) *
+				    Math.random()|0)
+		  .toString(16)});
 	item.data('order', max);
 	if (cssClass!=='nb-final') {
 	    item.append(moveMaker(item));
@@ -123,17 +164,15 @@ $(document).ready(function() {
 	    }
 	);
         $('#notebook').prepend(item);
-        max += -100000;
+        max += SUBTRACT;
     }
     
     $('#insert').on('click', function(e) {insertItem();});
 
     
     insertFinal();
-    insertItem();
-    insertItem();
-    insertItem();
-    insertItem();
-    insertItem();
-    insertItem();
+    for (var i = 0; i<20; i++) {
+	insertItem();
+    }
+
 });
