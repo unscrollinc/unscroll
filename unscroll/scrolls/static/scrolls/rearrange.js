7e24429490d32d2438eaebb95573793592f07cfe
Paul Ford <ford@ftrain.com>
2017-04-26 16:26:29 -0400

    Continuing on editor funcitons.

--- unscroll/scrolls/static/scrolls/rearrange.js
+++ unscroll/scrolls/static/scrolls/rearrange.js
@@ -9,7 +9,7 @@
 })(jQuery);
 
 $(document).ready(function() {
-    var max = 200.0;
+    var max = 100.0;
     const SUBTRACT = -1;
     var moving = false;
     var elToMove = undefined;
@@ -24,10 +24,11 @@ $(document).ready(function() {
 		       'title':'Rearrange notes'})
 	    .html('M');
 	
-	mover.on('click', function(e) {
+	
+	mover.on('click', function(ev) {
 	    if(!moving) {
-		e.stopPropagation();
-		el.css({'opacity':0.1});
+		ev.stopPropagation();
+		el.addClass('selected');
 		$('.mover')
 		    .html('R')
 		    .on('click', function(ev) {
@@ -38,8 +39,8 @@ $(document).ready(function() {
 			    var els = $(elToMove)
 				.nextUntil(p)
 				.add(p)
-				.addBack()			    
-				.css({'opacity':0.1});
+				.addBack()
+				.addClass('selected');
 			    elsToMove.push.apply(elsToMove, els);
 			}
 			if (p.isBefore(elToMove)) {
@@ -48,8 +49,9 @@ $(document).ready(function() {
 				.nextUntil(elToMove)
 				.add(p)
 				.addBack()			    
-				.css({'opacity':0.1});
-			    elsToMove.push.apply(els, elsToMove);			    
+				.addClass('selected');
+			    elsToMove.push.apply(els, elsToMove);
+			    addUnselect(elsToMove);			    
 			}
 			console.log('Clicked on range. from:',
 				    elToMove.data('order'),
@@ -71,55 +73,61 @@ $(document).ready(function() {
     function addClicker(item) {
 	item.on('click', function(e){
 	    if (moving) {
-		var ct = elsToMove.length;
-		if (ct > 0) {
-		    var rightBefore = item.prev().data('order');
-		    var rightAfter = item.data('order');
-		    console.log('count is', ct);
-		    for (var i = 0; i<ct; i++) {
-			var el = $(elsToMove[i]);
-			var between = rightBefore + ((i + 1) * ((rightAfter - rightBefore)/(1 + ct)));
-			old_order = el.data('order');
-			el.data('order', between);
-			el.children('p.no').remove();
-			el.prepend('<p class="no"><b>X' + between + '</b></p>');
-			el.insertBefore(item);
-			el.css({'opacity':'1'});
-
-			console.log('EACH: PATCH this note with ID X with an update that the order',
-				    old_order, 'is now', between);
-
-		    }
-			
-		    $('.mover').html('M');			
-		    moving = false;		
-		    elToMove = undefined;
+		if (item.hasClass('selected')) {
+		    $('.selected').removeClass('selected');
+		    console.log('all good, all done');
 		    elsToMove = [];
-
-		    
+		    elToMove = undefined;
 		}
 		else {
-		    if (elToMove !== item) {
-			var old_id = elToMove.data('order');
+		    var ct = elsToMove.length;
+		    if (ct > 0) {
 			var rightBefore = item.prev().data('order');
 			var rightAfter = item.data('order');
-			var between = rightBefore + (rightAfter - rightBefore)/2;			    
-			if (rightBefore===undefined) {
-			    between = rightAfter - 1;
+			console.log('count is', ct);
+			for (var i = 0; i<ct; i++) {
+			    var el = $(elsToMove[i]);
+			    var between = rightBefore + ((i + 1) * ((rightAfter - rightBefore)/(1 + ct)));
+			    old_order = el.data('order');
+			    el.data('order', between);
+			    el.children('p.no').remove();
+			    el.prepend('<p class="no"><b>X' + between + '</b></p>');
+			    el.insertBefore(item);
+			    el.removeClass('selected');
+			    
+			    console.log('EACH: PATCH this note with ID X with an update that the order',
+					old_order, 'is now', between);
+			    
 			}
-			elToMove.data('order', between);
-			elToMove.children('p.no').remove();
-			elToMove.prepend('<p class="no"><b>' + between + '</b></p>');
-			elToMove.insertBefore(item);
+			
+			$('.mover').html('M');			
+			moving = false;		
+			elToMove = undefined;
+			elsToMove = [];
 		    }
-		    elToMove.css({'opacity':'1'});
-		    $('.mover').html('M');
-		    $('.mover').html('M');			
-		    moving = false;		
-		    elToMove = undefined;
-		    elsToMove = [];			
-
-		    console.log('PATCH this note with ID X with an update that the order', old_id, 'is now', between);
+		    else {
+			if (elToMove !== item) {
+			    var old_id = elToMove.data('order');
+			    var rightBefore = item.prev().data('order');
+			    var rightAfter = item.data('order');
+			    var between = rightBefore + (rightAfter - rightBefore)/2;			    
+			    if (rightBefore===undefined) {
+				between = rightAfter - 1;
+			    }
+			    elToMove.data('order', between);
+			    elToMove.children('p.no').remove();
+			    elToMove.prepend('<p class="no"><b>' + between + '</b></p>');
+			    elToMove.insertBefore(item);
+			}
+			elToMove.removeClass('selected');
+			$('.mover').html('M');
+			$('.mover').html('M');			
+			moving = false;		
+			elToMove = undefined;
+			elsToMove = [];			
+			
+			console.log('PATCH this note with ID X with an update that the order', old_id, 'is now', between);
+		    }
 		}
 
 	    }
@@ -131,7 +139,6 @@ $(document).ready(function() {
     }
     
     function insertItem(cssClass, count, text) {
-
 	if (count===undefined) {
 	    count = max;
 	}
@@ -141,21 +148,20 @@ $(document).ready(function() {
 	if (text===undefined) {
 	    text = '<p class="no"><b>' + count + '</b></p>';
 	}
+	console.log("#"+((1<<24)* Math.random()|0));
 	var item = $('<div></div>',
 		     {'class':cssClass})
 		     .html(text)
-	    .css({'background':"#"+((1<<24) *
-				    Math.random()|0)
+	    .css({'background':"#"+((1<<24) * Math.random()|0)
 		  .toString(16)});
 	item.data('order', max);
 	if (cssClass!=='nb-final') {
 	    item.append(moveMaker(item));
 	}
 	addClicker(item);
-	
 	item.hover(
 	    function(e){
-		if(moving) {
+		if (moving) {
 		    item.css({cursor:'hand'});
 		}
 	    },
@@ -166,10 +172,7 @@ $(document).ready(function() {
         $('#notebook').prepend(item);
         max += SUBTRACT;
     }
-    
     $('#insert').on('click', function(e) {insertItem();});
-
-    
     insertFinal();
     for (var i = 0; i<20; i++) {
 	insertItem();
