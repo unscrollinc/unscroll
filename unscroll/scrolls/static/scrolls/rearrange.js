9ac7fdca9ced4d4c77b2253ae1f62f4c67d35443
Paul Ford <ford@ftrain.com>
2017-05-06 13:45:29 -0400

    Basically working core notebook interface. Time to link things back together.

--- unscroll/scrolls/static/scrolls/rearrange.js
+++ unscroll/scrolls/static/scrolls/rearrange.js
@@ -44,16 +44,14 @@
             var ctr = max;
 
 	    function makeInsertionTemplate() {
-		return {moving:false, from:undefined, to:undefined, target:undefined };
+		return {moving:false, from:undefined, to:undefined};
 	    }
             
-            this.decmax = function() {
+            this.decmin = function() {
                 ctr = ctr - dec;
                 return ctr;
             }
 
-	    
-	    
             var nb = this;
 
             this.id = id;
@@ -98,29 +96,81 @@
                 targetBlank: true                
             });
 
-	    this.reorderItems = function(from, to) {
+	    // This is where we reorder items.
+	    this.reorderItems = function(target) {
 		var replace = new Array();
 		
+		var ordered = this.fixOrder(this.insertion);
+		
+		console.log(ordered);
+
+		var to_loc = undefined;
+		var from_loc = undefined;		
+		var target_loc = undefined;
+		
 		for (var i = 0; i<this.items.length; i++) {
-		    if (this.items[i] == from) {
+		    if (this.items[i] == ordered.from) {
+			from_loc = i;
 		    }
-		    else if (this.items[i] == to) {
-			var ci = this.items[i];
+		    if (this.items[i] == ordered.to) {
+			to_loc = i;
+		    }
+		    if (this.items[i] == target) {
+			target_loc = i;
+		    }		    		    
+		}
+		// If to is undefined we have a range of one
+		if (!ordered.to) {
+		    to_loc = from_loc;
+		}
+		
+		console.log('LOCS: ', from_loc, to_loc, target_loc);
+		for (var i = 0; i<this.items.length; i++) {
+		    if (i == target_loc) {
+			// Paste them in here
 			if (i==0) {
-			    from.order = ci.order - dec;
+			    for (var j = to_loc; j>=from_loc; j--) {
+				console.log('XXX',j);
+				this.items[j].order = this.decmin();
+			    }
 			}
 			else {
 			    var oneback = this.items[i-1];
-			    from.order = oneback.order + (ci.order - oneback.order)/2;
+			    for (var j = from_loc; j<=to_loc; j++) {
+				console.log('YYY',j);
+				var between = this.items[i].order - oneback.order;
+				var div = to_loc - from_loc + 2;
+				var eq = this.items[i].order + ' + ' + between + '/' + div;
+				console.log(eq);
+				this.items[j].order = oneback.order + between/div;
+				
+			    }
 			}
-			replace.push(from);
+			
+			for (var j=from_loc; j<=to_loc; j++) {
+			    this.items[j].needsUpdated = true;
+			    $(target.notebookView).before(this.items[j].notebookView);
+			    $(target.textView).before(this.items[j].textView);
+			    replace.push(this.items[j]);
+			    console.log('JJJJJJJJ',$(target.notebookView),this.items[j].notebookView);
+			    
+			}
 			replace.push(this.items[i]);
 		    }
-		    else {
-			replace.push(this.items[i]);			
+		    else if (i >= from_loc && i <= to_loc) {
+			// nothing
 		    }
+		    else {
+			replace.push(this.items[i]);
+		    }
+		    
 		}
-		return replace;
+		console.log(this.insertion);
+		for (i in replace) {
+		    console.log(replace[i].order, replace[i].note);
+		}
+		this.insertion = makeInsertionTemplate();
+		this.items = replace;
 	    }
 
 	    this.getNext = function(item) {
@@ -137,16 +187,30 @@
 		    }
 		}
 	    }
+	    
+	    // You can select a range bottom up. If that happens
+	    // switch "from" and "to".
+	    this.fixOrder = function(insertion) {
+		var _from = insertion.from;
+		var _to = insertion.to;
 
+		if (_to) {
+		    if (_from.order > _to.order) {
+			_from = _to;
+			_to = _from;
+		    }
+		}
+		
+		return {from:_from, to:_to}
+	    }
+
+	    // You can move a range of notes at once.
 	    this.getRange = function(insertion) {
 		console.log(insertion);
 		var span = new Array();
-		var _from = insertion.from;
-		var _to = insertion.to;
-		if (_from.order > _to.order) {
-		    _from = _to;
-		    _to = _from;
-		}
+		var ordered = this.fixOrder(insertion);
+		var _from = ordered.from;
+		var _to = ordered.to;
 		var started = false;
 		var finished = false;
 		for (var i = 0; i<this.items.length; i++) {
@@ -183,32 +247,21 @@
                 var item = new NotebookItem(kind, event, note);
                 this.dom_nb.prepend(item.notebookView);
                 this.dom_essay.prepend(item.textView);
-		item.order = this.decmax();
+		item.order = this.decmin();
 		
-		var mover = $('<span></span>',
-			      {class:'button mover'})
+		item.mover = $('<span></span>',
+			       {class:'button mover'})
 		    .html('MV');
 
 		item.notebookView.children('.top, .editable').on('click', function(ev){
 		    if (nb.insertion.moving) {
 			ev.stopImmediatePropagation();
 			ev.preventDefault();
-			var from = nb.insertion.from;
-			var to = item;
 			
-			var nb_from = from.notebookView;
-			var nb_to = $(item.notebookView);
-			nb_to.before(nb_from);
+			nb.insertion.from.buttons.children('.mover').removeClass('active');
 			
-			var text_from = from.textView;
-			var text_to = $(item.textView);			
-			text_to.before(text_from);
-
-			nb.items = nb.reorderItems(from, to);
-			from.buttons.children('.mover').removeClass('active');
-			from.needsUpdated = true;
+			nb.reorderItems(item);
 			$('.mover').html('MV');
-			nb.insertion = makeInsertionTemplate();
 		    }
 		});
 		
@@ -218,24 +271,21 @@
 			    && item != nb.insertion.from) {
 			    var el = $(item.notebookView);
 			    el.children()
-				.css({background:'lightgray',
-				      cursor:'hand'})
+				.css({cursor:'hand'});
 			}
 		    },
 		    function(ev) {
 			console.log(nb.moving);
 			if (!nb.moving) {
 			    var el = $(item.notebookView)			
-			    el.children().css({background:'white',
-					       cursor:'default'
-					      });
+			    el.children().css({cursor:'default'});
 			}
 		    });
 		
-		mover.on('click', function(ev) {
+		item.mover.on('click', function(ev) {
 		    if (nb.insertion.moving) {
 			if (item == nb.insertion.from) {
-			    mover.removeClass('active');
+			    item.mover.removeClass('active');
 			    nb.insertion = makeInsertionTemplate();
 			}
 			else {
@@ -249,14 +299,15 @@
 		    }
 		    else {
 			nb.insertion.moving = true;
-			mover.addClass('active');
+			item.mover.addClass('active');
 			$('.mover').not('.active').each(function(pos, el) {
 			    $(el).html('SET RANGE');
 			});
 			nb.insertion.from = item;
 		    }
 		})
-		item.buttons.append(mover);
+
+		item.buttons.prepend(item.mover);
 		this.items.unshift(item);
             }
 	    
@@ -315,9 +366,6 @@
         };
         
         var NotebookItem = function(kind, event, note) {
-
-
-	    
             var creationTime = new Date().getTime();
             var nbitem = this;
             this.event = event;
@@ -337,12 +385,17 @@
             this.needsDeleted = false;
 	    
 	    this.buttons = $('<div></div>', {class:'buttons'});
+	    this.mover = undefined;
+	    
             this.render = function() {
 		var d = $('<div></div>', {class:'top'});
                 if (this.event) {
                     return d.html(event.title);
                 }
-                else if (this.kind=='spacer') {
+                else if (this.kind=='closer') {
+		    return d.html('&mdash; FIN &mdash;')
+                }
+		else if (this.kind=='spacer') {
 		    this.buttons.append(
 			$('<span></span>', {class:'button'}).html('Space &times; 2')
 			    .addClass('active')
@@ -369,13 +422,13 @@
             }
 	    
             this.eventHTML = this.render();
-
+	    
 	    if (this.kind == 'spacer') {
 
             }
 	    
             this.editor = $('<div></div>', {class:'editable'});
-            if (this.kind === 'spacer') {
+            if (this.kind == 'spacer') {
                 this.editor.html($('<br/><br/>'));
             }
 	    else if (event && event.html) {
@@ -406,15 +459,21 @@
 		    nbitem.notebookView.remove();
 		    nbitem.textView.remove();		    
                 });	    
-
+	    
             this.makeNotebookView = function() {
-                var nbv = undefined;
-                return $('<div></div>',
-			 {class:'nb-item ' + kind})
-		    .append(this.buttons
-			    .append(this.deleteButton))
-                    .append(this.eventHTML)
-                    .append(this.editor);
+		if (this.kind != 'closer') {
+                    return $('<div></div>',
+			     {class:'nb-item ' + kind})
+			.append(this.buttons
+				.append(this.deleteButton))
+			.append(this.eventHTML)
+			.append(this.editor);
+		}
+		else {
+                    return $('<div></div>',
+			     {class:'nb-item ' + kind})
+			.append(this.eventHTML);
+		}
             }
             
             this.notebookView = this.makeNotebookView();
@@ -441,10 +500,10 @@
         var notebook = new Notebook('a');
 	
 	var lorem = 'Contrary to popular belief.';
-
+	notebook.makeItem('closer');
 	for (var i = 0; i<10; i++) {
             var item = notebook.makeItem('default',
-					 {title:i + '. Brisket hell ipsum dolor est', html:i + '. ' + lorem}, {title:"PANTS"});
+					 {title:i + '. Brisket hell ipsum dolor est', html:i + '. ' + lorem}, {title:i + "PANTS"});
 	}
     });
     
