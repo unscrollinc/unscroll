1c611b42e93eb82c28152227dc7d3eaed25e985a
Paul Ford <ford@ftrain.com>
2017-05-17 14:19:29 -0400

    Git importer

--- unscroll/bin/git2api.py
+++ unscroll/bin/git2api.py
@@ -1,10 +1,13 @@
 import subprocess
 import json
 import re
+import pprint
 
 COMMAND = 'git log --format="%cI%x09%H%x09%an%x09%s"'
 
-output = subprocess.run(COMMAND, shell=True, stdout=subprocess.PIPE, 
+output = subprocess.run(COMMAND,
+                        shell=True,
+                        stdout=subprocess.PIPE,
                         universal_newlines=True)
 
 a = []
@@ -13,8 +16,9 @@ for line in lines:
     els = re.split(r"\t+", line)
     if (len(els) is 4):
         date, _hash, name, subject = els
-        a.append({'datetime':date, 'tk':_hash, 'title':"{} ({})".format(subject, name)})
+        a.append({'datetime': date,
+                  'tk': _hash,
+                  'title': "{} ({})".format(subject, name)})
 
-
-print(json.dumps(a, sort_keys=True, indent=4))
+pprint.pprint(json.dumps(a, sort_keys=True, indent=4))
 
