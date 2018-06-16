709982190d00ba3bf8fe5ad3fbf8b207d7236ec6
Paul Ford <ford@ftrain.com>
2018-06-15 21:19:53 -0400

    dpla import

--- scratchpad/dpla/DOIT.sh
+++ scratchpad/dpla/DOIT.sh
@@ -1,8 +1,9 @@
 DB=dpla.db
+FILE=~/Downloads/all.json.gz
 rm $DB ;
 cat db.sql | sqlite3 $DB ;
-echo "begin transaction; " |sqlite3 $DB
-pv -cN source < ~/Dropbox/all.json.gz | gzcat | sed '1d;$d' | perl -ne "s/^,//; s/'/''/g; s/(.*)$/insert into data values ('\$1');/;print;" | sqlite3 $DB ;
-echo "commit; " | sqlite3 $DB
+echo "BEGIN TRANSACTION; " |sqlite3 $DB
+pv -cN source < $FILE | zcat | sed '1d;$d' | perl -ne "s/^,//; s/'/''/g; s/(.*)$/INSERT INTO data VALUES ('\$1');/; print;" | sqlite3 $DB ;
+echo "COMMIT; " | sqlite3 $DB
 
 
