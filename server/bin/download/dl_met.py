81ca0ea3242917b0d5ba9a1310540f1187ede9f6
Paul Ford <ford@ftrain.com>
2018-08-01 16:28:00 -0400

    met tidy

--- server/bin/download/dl_met.py
+++ server/bin/download/dl_met.py
@@ -55,23 +55,6 @@ def save_met():
     scroll = c.create_or_retrieve_scroll('The Met')
     s = requests.Session()
 
-    # headers={'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
-
-
-    # cookies = {'visid_incap_1661977':'qwcvau9oR867N5Om6Wumib30GlsAAAAAQUIPAAAAAADdoCHDnJviQGJrr54oP7Ke',
-    #            'visid_incap_1661922':'lCtv3coJTIeyF5DMIq1XbcL1GlsAAAAAQUIPAAAAAABhH0bWsAqc572sGggwR8Lm',
-    #            'optimizelyEndUserId':'oeu1528493508170r0.018484126855202065',
-    #            '_ga':'GA1.2.719654953.1528493509',
-    #            '__qca':'P0-1257828822-1528493509063',
-    #            '__utma':'85941765.719654953.1528493509.1528494961.1528494961.1',
-    #            '__utmz':'85941765.1528494961.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
-    #            'visid_incap_1662004':'Uth0bQ62RfqddEnycrXaVm6AQVsAAAAAQUIPAAAAAAAes3j0jOJamlarKhGZI+80',
-    #            'incap_ses_892_1661977':'W1oRLWvwFXZNs+5IsQVhDBf/Q1sAAAAA3a9zp+QggKGlFPnJOfE15Q==',
-    #            'incap_ses_892_1661922':'VoMVZQyiX0XZHgBJsQVhDIwJRFsAAAAAgbPVPixNAxVQfWR7JLQ9Ug==',
-    #            '_ceg.s':'pbkfd4',
-    #            '_ceg.u':'pbkfd4',
-    # }
-
     conn = sqlite3.connect('cache/met.db')
     conn.row_factory = sqlite3.Row
     
@@ -124,21 +107,3 @@ def __main__():
 
 
 __main__()
-            # except FileNotFoundError as e:
-            #     try:
-            #         print('getting image', img)
-            #         r = s.get(img, headers=headers, cookies=cookies, verify=False)
-            #         if r.status_code==200 and r.headers.get('content-type') == 'text/html':
-            #             sys.exit(0)
-            #         print(r.headers.get('content-type'))
-            #         p = pathlib.Path(local)
-            #         p.parent.mkdir(parents=True, exist_ok=True) 
-            #         f = open(local, 'wb')
-            #         f.write(r.content)
-            #         f.close()
-            #         found = True
-            #         time.sleep(0)                    
-            #     except ConnectionError as e:
-            #         print('[dl_met.py] ConnectionError: {}'.format(e,))
-            #     except requests.exceptions.MissingSchema as e:
-            #         print('[dl_met.py] Bogus image: {}'.format(e,))                    
